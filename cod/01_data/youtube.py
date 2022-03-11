03d6adb11102ba0e178fa599ee783ee4630ef329
ArturoSbr <soberonarturo@gmail.com>
2022-03-11 14:13:09 -0600

    Channel method done

--- cod/01_data/youtube.py
+++ cod/01_data/youtube.py
@@ -1,44 +1,97 @@
 # Libraries
+from ast import While
 import json
 import requests
 
 # Youtube class
 class channel():
-    # Initialize
-    def __init__(self):
+    # Initialize channel
+    def __init__(self, id=None, key=None):
+        self.id = id
+        self.key = key
         self.name = None
         self.joined = None
 
     # Get channel's title, join date and metrics
-    def statistics(self, channelId=None, key=None):
-        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id={channelId}&' + \
-            f'fields=items(statistics%2Csnippet%2Ftitle%2Csnippet%2FpublishedAt)&key={key}'
+    def get_statistics(self):
+        url = 'https://youtube.googleapis.com/youtube/v3/channels?' + \
+            f'part=statistics&part=snippet&id={self.id}&' + \
+            f'fields=items(statistics%2Csnippet%2Ftitle%2Csnippet%2FpublishedAt)&key={self.key}'
         r = json.loads(requests.get(url).text)
         self.name = r['items'][0]['snippet']['title']
         self.joinDate = r['items'][0]['snippet']['publishedAt'][:10]
         return r['items'][0]['statistics']
     
-    # Get channel's videoIds between two dates
-    def videos(self, key=None, channelId=None, date0=None, date1=None):
-        # TODO get next page until end
+    # Get channel's videos between two dates
+    def get_videos(self, date0=None, date1=None):
         date0 = date0.replace(':','%3A')
         date1 = date1.replace(':','%3A')
-        url = 'https://youtube.googleapis.com/youtube/v3/search?' + \
-            f'part=id&channelId={channelId}&maxResults=50&order=relevance&' + \
-            f'publishedAfter={date0}Z&publishedBefore={date1}Z&' + \
-            f'safeSearch=none&type=video&videoCategoryId=25&' + \
-            f'fields=items(id%2FvideoId)&key={key}'
-        return json.loads(requests.get(url).text)
+        ret = []
 
-"""
-https://youtube.googleapis.com/youtube/v3/search?part=id&
-part=snippet&
-channelId=UChqUTb7kYRX8-EiaN3XFrSQ&
-maxResults=50&order=relevance
-&publishedAfter=2021-11-09T00%3A00%3A00Z&
-publishedBefore=2021-11-09T23%3A59%3A59Z&
-safeSearch=none&
-type=video&
-fields=nextPageToken%2Citems(id%2FvideoId)%2Citems(snippet%2FpublishedAt)&
-key=[YOUR_API_KEY]
-"""
+        # Get first request
+        url = 'https://youtube.googleapis.com/youtube/v3/search?' + \
+            f'part=id&part=snippet&channelId={self.id}&maxResults=50&order=relevance&' + \
+            f'publishedAfter={date0}Z&publishedBefore={date1}Z&' + \
+            'safeSearch=none&type=video&videoCategoryId=25&' + \
+            f'fields=items(id%2FvideoId)%2Citems(snippet%2FpublishedAt)&key={self.key}'
+        print(url)
+        r = json.loads(requests.get(url).text)
+        try:
+            npt = r['nextPageToken']
+        except:
+            npt = None
+        for i in range(len(r['items'])):
+            ret.append((r['items'][i]['id']['videoId'], r['items'][i]['snippet']['publishedAt']))
+
+        # Get subsequent requests
+        while npt is not None:
+            url = 'https://youtube.googleapis.com/youtube/v3/search?' + \
+                f'part=id&part=snippet&channelId={self.id}&maxResults=50&order=relevance&' + \
+                f'publishedAfter={date0}Z&publishedBefore={date1}Z&' + \
+                f'safeSearch=none&type=video&videoCategoryId=25&pageToken={npt}' + \
+                f'fields=items(id%2FvideoId)%2Citems(snippet%2FpublishedAt)&key={self.key}'
+            print(url)
+            r = json.loads(requests.get(url).text)
+            try:
+                npt = r['nextPageToken']
+            except:
+                npt = None
+            for i in range(len(r['items'])):
+                ret.append((r['items'][i]['id']['videoId'], r['items'][i]['snippet']['publishedAt']))
+        
+        return ret
+
+class video():
+    # Initialize object
+    def __init__(self, id):
+        self.id = id
+
+    # Get video's details (title, tags, etc)
+    def get_details(self):
+        """
+        snippet
+            title
+            description
+            tags
+        contentDetails
+            duration
+        liveStreamingDetails
+            If None => Normal upload
+        """
+    
+    # Get video's top-level comments
+    def get_comments(self):
+        """
+        https://youtube.googleapis.com/youtube/v3/commentThreads?
+        part=snippet&
+        maxResults=50&
+        moderationStatus=published&
+        order=time&
+        textFormat=plainText&
+        videoId=uDjeOcBPxfk&
+        fields=
+            nextPageToken%2C%20
+            items(snippet%2FtopLevelComment%2Fsnippet%2FtextOriginal)%2C%20
+            items(snippet%2FtopLevelComment%2Fsnippet%2FpublishedAt)&
+        key=[YOUR_API_KEY]
+        """
